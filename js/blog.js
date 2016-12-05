ba51cf54beb1d4f623208a0d596e2ebd5acc51f2
Hector J. Vasquez <ipi.vasquez@gmail.com>
2016-12-05 12:42:57 -0600

    Markdown with showdown done!

--- js/blog.js
+++ js/blog.js
@@ -1,8 +1,30 @@
+var entriesBaseURL = 'entries/';
 
-var innings = [
-    'innings/1.txt',
-    'innings/2.txt'
-];
+function showEntry(evt) {
+    var entry = evt.srcElement.getAttribute('data-value');
 
-var container = document.getElementById('inContainer');
-container
+    get(entry, function (responseText) {
+        var converter = new showdown.Converter();
+
+        $('entry').innerHTML = converter.makeHtml(responseText);
+    });
+}
+
+function showEntries(responseText) {
+    var entries = JSON.parse(responseText);
+    var entriesContainer = $('entries');
+
+    for (var i = 0; i < entries.length; i++) {
+        var entry = e('a', entries[i]['name']);
+        entry.setAttribute('data-value', entriesBaseURL + entries[i]['href']);
+        entry.href = '#entry';
+        entry.addEventListener('click', showEntry);
+        entriesContainer.appendChild(e('li', entry));
+    }
+}
+
+function prepareShell() {
+    get(entriesBaseURL + 'entries.json', showEntries);
+}
+
+window.onload = prepareShell;
